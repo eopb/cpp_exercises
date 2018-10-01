e10bab67c3c7ee05d2cc16a341dce367db7f71e1
Ethan Brierley <ethanboxx@gmail.com>
2018-10-01 11:01:48 +0100

    using namespace std;

--- Chapter 2/convert_distances.cpp
+++ Chapter 2/convert_distances.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
     unsigned int yards{}, feet{}, inches{};
-    std::cout << "Enter a distance in yards, feet and inches"
-              << "with the three values separated by spaces" << std::endl;
-    std::cin >> yards >> feet >> inches;
+    cout << "Enter a distance in yards, feet and inches"
+         << "with the three values separated by spaces" << endl;
+    cin >> yards >> feet >> inches;
     const unsigned feet_per_yard{3};
     const unsigned inches_per_foot{12};
     unsigned total_inches{};
     total_inches = inches + inches_per_foot * (yards * feet_per_yard + feet);
-    std::cout << "The distance corresponds to " << total_inches << " inches.\n";
-    std::cout << "Enter a distance in inches: ";
-    std::cin >> total_inches;
+    cout << "The distance corresponds to " << total_inches << " inches.\n";
+    cout << "Enter a distance in inches: ";
+    cin >> total_inches;
     feet = total_inches / inches_per_foot;
     inches = total_inches % inches_per_foot;
     yards = feet / feet_per_yard;
     feet = feet % feet_per_yard;
-    std::cout << "The distance corresponds to "
-              << yards << " yards "
-              << feet << " feet "
-              << inches << " inches." << std::endl;
+    cout << "The distance corresponds to "
+         << yards << " yards "
+         << feet << " feet "
+         << inches << " inches." << endl;
     return 0;
 }
