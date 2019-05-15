37d8ef871910164220d765dc9f2570e6ad2e7282
Ethan Brierley <ethanboxx@gmail.com>
2019-05-15 12:09:31 +0100

    twos_complement 2

--- Chapter 3/twos_complement.cpp
+++ Chapter 3/twos_complement.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -8,8 +9,11 @@ int main()
     cin >> input_num;
     int invert_num{~input_num};
     int complement{invert_num + 1};
-    cout << "This is the number you inputted: " << input_num << endl;
-    cout << "This is the number inverted: " << invert_num << endl;
-    cout << "This is the number inverted with on added: " << complement << endl;
+    cout << "This is the number you inputted:            " << setfill('0') << hex << setw(8) << input_num << endl;
+    cout << "This is the number inverted:                " << setfill('0') << hex << setw(8) << invert_num << endl;
+    cout << "This is the number inverted with on added:  " << setfill('0') << hex << setw(8) << complement << endl;
+    cout << "This is the number you inputted:            " << dec << input_num << endl;
+    cout << "This is the number inverted:               " << dec << invert_num << endl;
+    cout << "This is the number inverted with on added: " << dec << complement << endl;
     return 0;
 }
